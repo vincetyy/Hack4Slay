cf3d28f9e94695d1fce2b00e731aa09e622b172c
vincetyy <vince.tanyy@gmail.com>
2024-02-06 20:35:51 +0800

    changed template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-
-const DEV_PORT = 2121;
+import react from "@astrojs/react";
+import bookshop from '@bookshop/astro-bookshop';
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.CI
-		? 'https://themesberg.github.io'
-		: `http://localhost:${DEV_PORT}`,
-	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
-
-	// output: 'server',
-
-	/* Like Vercel, Netlify,… Mimicking for dev. server */
-	// trailingSlash: 'always',
-
-	server: {
-		/* Dev. server only */
-		port: DEV_PORT,
-	},
-
-	integrations: [
-		//
-		sitemap(),
-		tailwind(),
-	],
+  site: "https://top-quail.cloudvent.net/",
+  integrations: [bookshop(), react()]
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,2 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-// https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript
-interface ImportMetaEnv {
-	readonly SITE: string;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
